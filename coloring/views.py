aeced6a4598dc32886cee294be424a5ed8b9471e
Verena Yiu <verenayiu@berkeley.edu>
2021-07-14 21:57:19 -0700

    added explore page

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def explore(request):
+    return render(request, 'coloring/explore.html')
